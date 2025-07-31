1d21d69f67c6958f344be5db4fc283ff3b3b4721
Sascha Frank <sfrank@wyl-online.de>
2025-07-31 06:46:51 +0200

    scss compile at reload

--- browsersync/bs-config.js
+++ browsersync/bs-config.js
@@ -1,7 +1,37 @@
+const { exec } = require("child_process");
+
 module.exports = {
   proxy: "nginx:80",
   host: "0.0.0.0",              // 👈 listen on all interfaces
-  files: ["/app/**/*.*"],
+  ignore: [
+    "config/bs-config.js",
+    "config/*.*",
+    "config/**/*.*",
+    "package-lock.json",
+    "package.json"
+  ],
+  files: [
+    "/app/**/*.*",
+  ],
+  callbacks: {
+    ready: function (err, bs) {
+      bs.emitter.on("file:changed", function (file) {
+        console.log("File changed:", file);
+        if (file.path.endsWith(".scss")) {
+          console.log("Rebuilding SCSS...");
+          console.log(bs.emitter)
+          exec("npm run build-scss", (err, stdout, stderr) => {
+            if (err) {
+              console.error(`SCSS build error: ${stderr}`);
+            } else {
+              console.log(`SCSS rebuilt:\n${stdout}`);
+            }
+          });
+        }
+      });
+    }
+  },
+
   watchOptions: {
     usePolling: true,
     interval: 1000
---
+++ browsersync/start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+npm install && \
+npm install -g sass@1.32.13 && \
+npm run watch-scss && \
+npm run start
